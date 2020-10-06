82a8d2cc32c969117405d2044c3fa670d839afd2
Katerina Solstad (katya sorok) <katya.solstad@gmail.com>
2020-10-06 16:53:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+# program for "Elecrtic Kart Racing"
+
+# rules for electric karting
+
+'''All drivers need to be at least 48 inches tall,
+   at least 6 years of age and be able to safely operate the karts.
+'''
+# age
+# height
+# is_safe_driver
+
+
+
+
+
+# program for "Pizza Party"
+
+'''
+    Your pizza cafe makes pizzas in size of 6 slices.
+    write a program that greets the user from your cafe and asks
+    how many people will be in their party.
+    when you get the answer, you should tell the user how many pizzas
+    they have to order.           
+'''
+# slices
+# pizzas
+# people
+
+
+
+
+# program "Will the water freeze in the yard?"
+
+''' Freezing temperature for water is 0 degrees Celsius.
+    Your tepmometer only gives you temperature in Fahrenheit
+'''
+#temperature conversion formulas
+#celsius=(fahrenheit-32)*(5/9)
+#fahrenheit=celsius*(9/5)+32
+# celsius
+# fahrenheit
+
+
+
+
+"""
+Boolean operations
+x or y   
+x and y
+not x
+not a==b is the same as not (a==b) (error!!!: a== not b) 
+<=
+>
+>=
+==
+!=
+
+#is (for objects)
+#is not (negated for objects)
+"""
+a = 9
+b = 7
+print(a == b)
+print(not (a == b))
+print(a != b)
+print(a is not b)
+
+"""True  and True   ->  True
+   True  and False  ->  False
+   False and True   ->  False
+   False and False  ->  False
+   
+   True  or False   ->  True
+   True  or True    ->  True
+   False or False   ->  False
+"""
